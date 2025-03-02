01310367fc2b6d53d163b5f923dfb10f3473f933
mko_io <me@mko.io>
2025-03-02 19:51:24 +1300

    Change the navigation to Chinese

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -3,77 +3,46 @@ import { Menu } from "@/types/menu";
 const menuData: Menu[] = [
   {
     id: 1,
-    title: "Home",
+    title: "主页",
     newTab: false,
     path: "/",
   },
   {
     id: 2,
-    title: "Features",
+    title: "关于",
     newTab: false,
     path: "/#features",
   },
   {
     id: 2.1,
-    title: "Blog",
+    title: "税务咨询",
     newTab: false,
     path: "/blog",
   },
   {
     id: 2.3,
-    title: "Docs",
+    title: "成功案例",
     newTab: false,
     path: "/docs",
   },
   {
     id: 3,
-    title: "Pages",
+    title: "教育培训",
     newTab: false,
     submenu: [
       {
         id: 31,
-        title: "Blog Grid",
+        title: "培训课程",
         newTab: false,
         path: "/blog",
       },
-      {
-        id: 34,
-        title: "Sign In",
-        newTab: false,
-        path: "/auth/signin",
-      },
-      {
-        id: 35,
-        title: "Sign Up",
-        newTab: false,
-        path: "/auth/signup",
-      },
-      {
-        id: 35,
-        title: "Docs",
-        newTab: false,
-        path: "/docs",
-      },
-      {
-        id: 35.1,
-        title: "Support",
-        newTab: false,
-        path: "/support",
-      },
-      {
-        id: 36,
-        title: "404",
-        newTab: false,
-        path: "/error",
-      },
     ],
   },
-
   {
     id: 4,
-    title: "Support",
+    title: "Book Online",
     newTab: false,
-    path: "/support",
+    path: "/book_online",
   },
 ];
 
